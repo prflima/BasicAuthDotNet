9038153112c85211d82910dd2038f0db6c2d8cc9
Paulo Ricardo Lima <p22.paulo@webmotors.com.br>
2021-03-25 18:17:46 -0300

    Basic auth finally

---
+++ BasicAuth/BasicAuth/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasicAuth.Controllers
+{
+	[Route("api/{controller}/{action}")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		[HttpGet]
+		[Authorize]
+		public IActionResult Index()
+		{
+			var username = HttpContext.User.Identity.Name;
+
+			return Ok($"Olá você foi autenticado, seu nome de usuário é: {username}");
+		}
+	}
+}
---
+++ BasicAuth/Core/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core
+{
+	public class User
+	{
+		public string Username { get; set; }
+		public string Password { get; set; }
+
+		public User()
+		{
+			Username = "Paulo";
+			Password = "testeauthwebmotors";
+		}
+	}
+}
